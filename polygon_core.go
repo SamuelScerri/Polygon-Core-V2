a87587a79bbc8b1b352ea2da96a938b4f53dc4a1
SamuelScerri <samuelscerri533@gmail.com>
2023-07-22 20:49:52 +0200

    Bug Fixes

--- polygon_core.go
+++ polygon_core.go
@@ -15,8 +15,13 @@ type Vertex4D struct {
 	x, y, z, w float32
 }
 
+type Vertex2D struct {
+	x, y float32
+}
+
 type Triangle struct {
 	vertices [3]Vertex4D
+	uv       [3]Vertex2D
 }
 
 type Buffer []byte
@@ -75,6 +80,8 @@ func (triangle *Triangle) multiplyMatrix(m2 *Matrix) (result Triangle) {
 	result.vertices[0] = tm1.convertToVertex()
 	result.vertices[1] = tm2.convertToVertex()
 	result.vertices[2] = tm3.convertToVertex()
+
+	result.uv = triangle.uv
 
 	return
 }
@@ -139,6 +146,13 @@ func (v1 *Vertex4D) interpolate(v2 *Vertex4D, factor float32) Vertex4D {
 	}
 }
 
+func (v1 *Vertex2D) interpolate(v2 *Vertex2D, factor float32) Vertex2D {
+	return Vertex2D{
+		v1.x*(1-factor) + v2.x*factor,
+		v1.y*(1-factor) + v2.y*factor,
+	}
+}
+
 func (triangle *Triangle) convertToNormalizedCoordinates() {
 	triangle.vertices[0].convertToNormalized()
 	triangle.vertices[1].convertToNormalized()
@@ -157,6 +171,10 @@ func (buffer *Buffer) clearScreen() {
 	}
 }
 
+func clamp(value, min, max int) int {
+	return int(math32.Max(math32.Min(float32(value), float32(max)), float32(min)))
+}
+
 func (triangle *Triangle) renderToScreen(buffer *Buffer, texture *Buffer, image *ebiten.Image) {
 	var vs1, vs2 Vertex4D = triangle.spanningVectors()
 	var span float32 = vs1.crossProduct(&vs2)
@@ -164,8 +182,8 @@ func (triangle *Triangle) renderToScreen(buffer *Buffer, texture *Buffer, image 
 	var minX, minY, maxX, maxY = triangle.bounds()
 
 	if span != 0 {
-		for x := minX; x < maxX; x++ {
-			for y := minY; y < maxY; y++ {
+		for x := clamp(minX, 0, width); x < clamp(maxX, 0, width); x++ {
+			for y := clamp(minY, 0, height); y < clamp(maxY, 0, height); y++ {
 				var convertedX, convertedY float32 = float32(x), float32(y)
 
 				var s, t, w float32 = triangle.barycentricCoordinates(&vs1, &vs2, &convertedX, &convertedY, &span)
@@ -173,9 +191,9 @@ func (triangle *Triangle) renderToScreen(buffer *Buffer, texture *Buffer, image 
 				if s >= 0 && t >= 0 && s+t <= 1 {
 					var location int = (y*width + x) * 4
 
-					var at Vertex4D = Vertex4D{0, 0, 1, triangle.vertices[0].w}
-					var bt Vertex4D = Vertex4D{2, 0, 1, triangle.vertices[1].w}
-					var ct Vertex4D = Vertex4D{2, 2, 1, triangle.vertices[2].w}
+					var at Vertex4D = Vertex4D{triangle.uv[0].x, triangle.uv[0].y, 1, triangle.vertices[0].w}
+					var bt Vertex4D = Vertex4D{triangle.uv[1].x, triangle.uv[1].y, 1, triangle.vertices[1].w}
+					var ct Vertex4D = Vertex4D{triangle.uv[2].x, triangle.uv[2].y, 1, triangle.vertices[2].w}
 
 					at.convertToNormalized()
 					bt.convertToNormalized()
@@ -189,7 +207,7 @@ func (triangle *Triangle) renderToScreen(buffer *Buffer, texture *Buffer, image 
 					var tx int = int(uvX * float32(image.Bounds().Dx()))
 					var ty int = int(uvY * float32(image.Bounds().Dy()))
 
-					var colorLocation int = ((ty*image.Bounds().Dx() + tx) * 4) % (image.Bounds().Dx() * image.Bounds().Dy() * 4)
+					var colorLocation int = ((ty*image.Bounds().Dx() + tx) * 4)
 
 					(*buffer)[location] = (*texture)[colorLocation]
 					(*buffer)[location+1] = (*texture)[colorLocation+1]
@@ -201,8 +219,9 @@ func (triangle *Triangle) renderToScreen(buffer *Buffer, texture *Buffer, image 
 
 }
 
-func clip_axis(vertices []Vertex4D, opposite, yAxis bool) (data []Vertex4D) {
+func clip_axis(vertices []Vertex4D, uv []Vertex2D, opposite, yAxis bool) (data []Vertex4D, uvData []Vertex2D) {
 	previousVertex := vertices[len(vertices)-1]
+	previousUV := uv[len(uv)-1]
 	previousInside := 0
 
 	previousComponent := previousVertex.x
@@ -212,17 +231,18 @@ func clip_axis(vertices []Vertex4D, opposite, yAxis bool) (data []Vertex4D) {
 	}
 
 	if opposite {
-		if previousComponent >= -previousVertex.w {
+		if previousComponent > -previousVertex.w {
 			previousInside = 1
 		}
 	} else {
-		if previousComponent <= previousVertex.w {
+		if previousComponent < previousVertex.w {
 			previousInside = 1
 		}
 	}
 
 	for v := 0; v < len(vertices); v++ {
 		currentVertex := vertices[v]
+		currentUV := uv[v]
 		currentInside := 0
 
 		currentComponent := currentVertex.x
@@ -235,11 +255,11 @@ func clip_axis(vertices []Vertex4D, opposite, yAxis bool) (data []Vertex4D) {
 		}
 
 		if opposite {
-			if currentComponent >= -currentVertex.w {
+			if currentComponent > -currentVertex.w {
 				currentInside = 1
 			}
 		} else {
-			if currentComponent <= currentVertex.w {
+			if currentComponent < currentVertex.w {
 				currentInside = 1
 			}
 		}
@@ -254,14 +274,19 @@ func clip_axis(vertices []Vertex4D, opposite, yAxis bool) (data []Vertex4D) {
 			}
 
 			data = append(data, previousVertex.interpolate(&currentVertex, factor))
+			uvData = append(uvData, previousUV.interpolate(&currentUV, factor))
+
+			//fmt.Println(uvData)
 		}
 
 		if currentInside == 1 {
 			data = append(data, currentVertex)
+			uvData = append(uvData, currentUV)
 		}
 
 		previousVertex = currentVertex
 		previousInside = currentInside
+		previousUV = currentUV
 	}
 
 	return
@@ -269,26 +294,29 @@ func clip_axis(vertices []Vertex4D, opposite, yAxis bool) (data []Vertex4D) {
 
 func (t *Triangle) clip() (triangles []Triangle) {
 	var vertices []Vertex4D = []Vertex4D{t.vertices[0], t.vertices[1], t.vertices[2]}
+	var uvdat []Vertex2D = []Vertex2D{t.uv[0], t.uv[1], t.uv[2]}
 
-	vertices = clip_axis(vertices, false, false)
+	fmt.Println(uvdat)
+
+	vertices, uvdat = clip_axis(vertices, uvdat, false, false)
 
 	if len(vertices) > 0 {
-		vertices = clip_axis(vertices, true, false)
+		vertices, uvdat = clip_axis(vertices, uvdat, true, false)
 
 		if len(vertices) > 0 {
-			vertices = clip_axis(vertices, false, true)
+			vertices, uvdat = clip_axis(vertices, uvdat, false, true)
 
 			if len(vertices) > 0 {
-				vertices = clip_axis(vertices, true, true)
+				vertices, uvdat = clip_axis(vertices, uvdat, true, true)
 			}
 		}
 	}
 
 	if len(vertices) > 0 {
 		for index := 0; index < len(vertices)-2; index++ {
-			triangles = append(triangles, Triangle{vertices: [3]Vertex4D{
-				vertices[0], vertices[index+1], vertices[index+2],
-			}})
+			triangles = append(triangles, Triangle{
+				vertices: [3]Vertex4D{vertices[0], vertices[index+1], vertices[index+2]},
+				uv:       [3]Vertex2D{uvdat[0], uvdat[index+1], uvdat[index+2]}})
 		}
 	}
 
@@ -298,10 +326,6 @@ func (t *Triangle) clip() (triangles []Triangle) {
 type Game struct{}
 
 func (g *Game) Update() error {
-	//basicTriangle.vertices[0].z -= .001
-	//basicTriangle.vertices[1].z -= .001
-	//basicTriangle.vertices[2].z -= .001
-
 	if ebiten.IsKeyPressed(ebiten.KeyRight) {
 		basicTriangle.vertices[0].x += .001
 		basicTriangle.vertices[1].x += .001
@@ -316,8 +340,8 @@ func (g *Game) Update() error {
 
 	if ebiten.IsKeyPressed(ebiten.KeyUp) {
 		basicTriangle.vertices[0].y += .001
-		//basicTriangle.vertices[1].y += .001
-		//basicTriangle.vertices[2].y += .001
+		basicTriangle.vertices[1].y += .001
+		basicTriangle.vertices[2].y += .001
 	}
 
 	if ebiten.IsKeyPressed(ebiten.KeyDown) {
@@ -396,9 +420,13 @@ func main() {
 
 	projectionMatrix = createProjectionMatrix(90, aspectRatio, .1, 1000)
 
-	basicTriangle.vertices[0] = Vertex4D{0, .5, -2, 1}
-	basicTriangle.vertices[1] = Vertex4D{-.5, -.5, -2, 1}
-	basicTriangle.vertices[2] = Vertex4D{.5, -.5, -2, 1}
+	basicTriangle.vertices[0] = Vertex4D{0, -.5, -2, 1}
+	basicTriangle.vertices[1] = Vertex4D{-.5, .5, -2, 1}
+	basicTriangle.vertices[2] = Vertex4D{.5, .5, -2, 1}
+
+	basicTriangle.uv[0] = Vertex2D{0, 0}
+	basicTriangle.uv[1] = Vertex2D{1, 0}
+	basicTriangle.uv[2] = Vertex2D{1, 1}
 
 	if err := ebiten.RunGameWithOptions(&Game{}, &ebiten.RunGameOptions{GraphicsLibrary: ebiten.GraphicsLibraryOpenGL, InitUnfocused: false, ScreenTransparent: false, SkipTaskbar: false}); err != nil {
 		log.Fatal(err)
